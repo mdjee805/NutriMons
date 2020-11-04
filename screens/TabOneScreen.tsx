151d446fe996c1e4de010a727da5dac3aa30417f
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-11-03 17:21:58 -0800

    title and bars

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -5,27 +5,59 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Dashboard</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.js" />
-    </View>
+    return (
+        
+        <View style={styles.container}>
+
+      <View style={styles.bluebox}>
+       <Text style={styles.title}>NutriMons</Text>
+      </View>
+      
+      <Text style={styles.title1}>Dashboard</Text>
+
+      <View style={{width: 3000, height: 30, alignItems: 'center',
+                backgroundColor: 'blue'}}>
+            <Text style={{ fontSize: 20, fontWeight: 'normal', color: 'white'}}>Today</Text>
+      </View>
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            <EditScreenInfo path="/screens/TabOneScreen.js" />
+        
+
+            <Text style={styles.graphTitle}>
+                Nutrient Overview</Text>
+        </View>
+      
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
+
+        alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
+    fontSize: 50,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+    title1: {
+        fontSize: 30,
+        fontWeight: 'normal',
+    },
+    graphTitle: {
+        marginVertical: 800,
+        fontSize: 30,
+        fontWeight: 'normal',
+    },
+    bluebox: {
+        width: 3000,
+        height: 80,
+        alignItems: 'center',
+        backgroundColor: 'blue'
+    },
   separator: {
-    marginVertical: 30,
+    
     height: 1,
     width: '80%',
   },
