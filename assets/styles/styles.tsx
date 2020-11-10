b9fad0fa5f786817653ca68f7b22876d7a9e3b8d
MouseMan2 <jeemouseman2@gmail.com>
2020-11-09 18:10:53 -0800

    profile screen

--- assets/styles/styles.tsx
+++ assets/styles/styles.tsx
@@ -4,32 +4,35 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-around',
+        flexDirection: 'column', 
+        justifyContent: 'space-around'
     },
     rowContainer: { //add a flex
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
+        flexDirection: 'row', //children placed in a row
+        justifyContent: 'space-around', //main axis (row here)
+        alignItems: 'center' //cross axis (column here)
     },
     itemContainer: {
         flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
         backgroundColor: "lightblue"
     },
     title: {
         fontSize: 20,
-        fontWeight: 'bold',
+        fontWeight: 'bold'
     },
     separator: {
         marginVertical: 30,
         height: 1,
-        width: '80%',
+        width: '80%'
     },
 
     minhsContainer: {
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'flex-start',
+        justifyContent: 'flex-start'
     },
     bluebox: {
         flex: .1,
@@ -37,9 +40,6 @@ export default StyleSheet.create({
     },
     graphTitle: {
         fontSize: 30,
-        fontWeight: 'normal',
+        fontWeight: 'normal'
     },
-
-    
-   
 });
--- screens/TabProfileScreen.tsx
+++ screens/TabProfileScreen.tsx
@@ -7,26 +7,173 @@ import styles from "../assets/styles/styles";
 
 export default function ProfileScreen() {
     return (
-        <View style={styles.container}>
-            <View style={styles.rowContainer}>
-                <View style={styles.itemContainer}>
-                    <Image style={{ width: 40, height: 40 }} source={require('../assets/images/profilePic.png')} />
-                </View>
+        <View style={[styles.container, {}]}>
+
+            {//Title
+            }
+            <View style={[styles.rowContainer, { flex: .1 }]}>
                 <View style={styles.itemContainer}>
                     <Text style={styles.title}>ProfileScreen</Text>
                 </View>
-                <View style={styles.itemContainer}>
-                    <Image style={{}} source={require('../assets/images/tamaPic.png')} />
+            </View>
+
+            {//Profile Icon
+            }
+            <View style={[styles.rowContainer, { flex: .15, backgroundColor: 'lightblue', justifyContent: 'flex-start', alignItems: 'flex-start' }]}>
+                <View style={[styles.itemContainer, { marginTop: 10, marginBottom: 10, marginLeft: 10, flex: .1, justifyContent: 'flex-start', alignItems: 'flex-start' }]}>
+                    <Image style={{ width: 40, height: 40 }} source={require('../assets/images/profilePic.png')} />
+                    <Image style={{ width: 10, height: 10, margin: 5, justifyContent: 'flex-start', alignItems: 'flex-start' }} source={require('../assets/images/pencil.png')} />
                 </View>
             </View>
-            <View style={styles.rowContainer}>
-                <Image style={{ width: 180, height: 120 }} source={require('../assets/images/splash.png')} />
+            
+            <View style={[styles.rowContainer, { flex: .25, backgroundColor: 'lightblue', alignItems: 'flex-start', justifyContent: 'space-between' }]}>
+
+                {//Personal
+                }
+                <View style={[styles.container, { flex: .3, margin:10, alignItems: 'flex-start', justifyContent: 'flex-start' }]}>
+                    <View style={styles.rowContainer}>
+                        <Text style={{ fontWeight: 'bold' }}>
+                            Personal
+                          </Text>
+                         <Image style={{ width: 10, height: 10, margin: 5 }} source={require('../assets/images/pencil.png')} />
+                    </View>
+                    <View>
+                        <Text>
+                            Name
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Username
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Password
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Birthday
+                        </Text>
+                    </View>
+                </View>
+
+                {//Financial
+                }
+                <View style={[styles.container, { flex: .3, margin: 10, alignItems: 'flex-start', justifyContent: 'flex-start' }]}>
+                    <View style={styles.rowContainer}>
+                        <Text style={{ fontWeight: 'bold' }}>
+                            Financial
+                          </Text>
+                        <Image style={{ width: 10, height: 10, margin: 5 }} source={require('../assets/images/pencil.png')} />
+                    </View>
+                    <View>
+                        <Text>
+                            Source
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            History
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Subscriptions
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            NutriCoins
+                        </Text>
+                    </View>
+                </View>
+
             </View>
-            <View style={styles.rowContainer}>
-                <Text>button1</Text>
-                <Text>button2</Text>
-                <Text>button3</Text>
+
+            <View style={[styles.rowContainer, { flex: .6, backgroundColor: 'lightblue', alignItems: 'flex-start', justifyContent: 'space-between' }]}>
+
+                {//Health Factors
+                }
+                <View style={[styles.container, { flex: .4, margin: 10, alignItems: 'flex-start', justifyContent: 'flex-start' }]}>
+                    <View style={styles.rowContainer}>
+                        <Text style={{ fontWeight: 'bold' }}>
+                            Age: 68
+                          </Text>
+                        <Image style={{ width: 10, height: 10, margin: 5 }} source={require('../assets/images/pencil.png')} />
+                    </View>
+                    <View>
+                        <Text>
+                            Sex: M
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Weight: 280 lbs
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Height: 5'8''
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Ethnicity: White
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Diet: Vegan
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            Health History:
+                        </Text>
+                        <Text style={{ fontSize: 12 }}>
+                            Diabetes
+                        </Text>
+                        <Text style={{ fontSize: 12 }}>
+                            Hypertension
+                        </Text>
+                    </View>
+                </View>
+
+                {//Health Goals
+                }
+                <View style={[styles.container, { flex: .6, margin: 10, alignItems: 'flex-start', justifyContent: 'flex-start' }]}>
+                    <View style={styles.rowContainer}>
+                        <Text style={{ fontWeight: 'bold' }}>
+                            Health Goals
+                          </Text>
+                        <Image style={{ width: 10, height: 10, margin: 5 }} source={require('../assets/images/pencil.png')} />
+                    </View>
+                    <View>
+                        <Text>
+                            lose 100 lbs
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            maintain nutrient health
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            walk 2 miles each day
+                        </Text>
+                    </View>
+                    <View>
+                        <Text>
+                            
+                        </Text>
+                    </View>
+                </View>
+
             </View>
+
         </View>
     );
 }
