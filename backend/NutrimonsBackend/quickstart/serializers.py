9e205e86837fe4da429580fe07aa94b43fec2a28
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2021-01-26 18:36:36 -0800

    hello

--- backend/NutrimonsBackend/quickstart/serializers.py
+++ backend/NutrimonsBackend/quickstart/serializers.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User, Group
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['id','url', 'username','password', 'email', 'groups']
